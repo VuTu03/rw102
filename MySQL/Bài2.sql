0d9cc9aab75389528179826c01ebcb1e7f447a55
VuTu03 <vuvantu08072003@gmail.com>
2026-08-16 22:57:31 +0700

    Nộp bài tập 2

--- MySQL/Bài2.sql
+++ MySQL/Bài2.sql
@@ -21,13 +21,13 @@ INSERT INTO Department(department_name)
 
 
 -- Table 2: Potision
-DROP TABLE IF EXISTS Potision;
-CREATE TABLE Potision(
-Potision_id 	INT PRIMARY KEY AUTO_INCREMENT,
-Potision_name 	ENUM('DEV','Test','Scrum Master','PM','BA')
+DROP TABLE IF EXISTS Position;
+CREATE TABLE `Position`(
+Position_id 	INT PRIMARY KEY AUTO_INCREMENT,
+Position_name 	ENUM('DEV','Test','Scrum Master','PM','BA')
 );
-SELECT * FROM Potision;
-INSERT INTO Potision(Potision_name)
+SELECT * FROM Position;
+INSERT INTO `Position`(Position_name)
 	VALUES 	('DEV'),
 			('Test'),
             ('Scrum Master'),
@@ -45,8 +45,8 @@ Full_name 		VARCHAR(50),
 Department_id 	INT,
 Position_id 	INT,
 Create_date 	DATETIME DEFAULT CURRENT_TIMESTAMP,
-constraint fk_ac_dep foreign key (Department_id) references Department(Department_id),
-constraint fk_ac_pos foreign key (Position_id) references `Position`(Position_id)
+CONSTRAINT fk_acc_Dep FOREIGN KEY (Department_id) REFERENCES Department(Department_id),
+CONSTRAINT fk_acc_Pos FOREIGN KEY (Position_id) REFERENCES `Position`(Position_id)
 );
 SELECT * FROM Account;
 INSERT INTO Account (Email, User_name, Full_name, Department_id, Position_id)
@@ -79,7 +79,8 @@ DROP TABLE IF EXISTS GroupAccount;
 CREATE TABLE GroupAccount(
 Group_id 	INT,
 Account_id	INT,
-Join_date 	DATETIME DEFAULT CURRENT_TIMESTAMP
+Join_date 	DATETIME DEFAULT CURRENT_TIMESTAMP,
+CONSTRAINT fk_Group_Gro FOREIGN KEY (Group_id) REFERENCES `Group`(Group_id)
 );
 SELECT * FROM GroupAccount;
 INSERT INTO GroupAccount (Group_id, Account_id)
@@ -95,12 +96,15 @@ VALUES (1, 45),
 DROP TABLE IF EXISTS TypeQuestion;
 CREATE TABLE TypeQuestion(
 Type_id 	INT PRIMARY KEY AUTO_INCREMENT,
-Type_name 	ENUM('Essay','Multiple-Choice')
+Type_name 	ENUM('Essay','Multiple-Choice', 'True-False','Fill-in-the-Blank','Short-Answer')
 );
 SELECT * FROM TypeQuestion;
 INSERT INTO TypeQuestion(Type_name)
 	VALUES 	('Essay'),
-			('Multiple-Choice');
+			('Multiple-Choice'),
+            ('True-False'),
+            ('Fill-in-the-Blank'),
+            ('Short-Answer');
 
 
 
@@ -124,15 +128,17 @@ Content 		VARCHAR(100),
 Category_id 	INT,
 Type_id 		INT,
 Creator_id 		INT,
-Create_date 	DATETIME DEFAULT CURRENT_TIMESTAMP
+Create_date 	DATETIME DEFAULT CURRENT_TIMESTAMP,
+CONSTRAINT fk_ques_Cat FOREIGN KEY (Category_id) REFERENCES CategoryQuestion(Category_id),
+CONSTRAINT fk_ques_Typ FOREIGN KEY (Type_id) REFERENCES TypeQuestion(Type_id)
 );
 SELECT * FROM Question;
 INSERT INTO Question(Content, Category_id, Type_id, Creator_id)
-	VALUES ('Lập trình là gì?', 1, 3, 33),
+	VALUES ('Lập trình là gì?', 1, 2, 33),
 			('Biến trong lập trình dùng để làm gì?', 2, 5, 14),
-            ('Vòng lặp có tác dụng gì?',3, 8, 21),
-            ('Hàm (Function) là gì?', 4, 6, 15),
-            ('SQL dùng để làm gì?', 5, 4, 32);
+            ('Vòng lặp có tác dụng gì?',3, 4, 21),
+            ('Hàm (Function) là gì?', 4, 3, 15),
+            ('SQL dùng để làm gì?', 5, 1, 32);
     
     
 
@@ -144,7 +150,8 @@ CREATE TABLE Answer(
 Answer_id 	INT PRIMARY KEY AUTO_INCREMENT,
 Content 	VARCHAR(100),
 Question_id INT,
-isCorrect 	ENUM('Đúng','Sai')
+isCorrect 	ENUM('Đúng','Sai'),
+CONSTRAINT fk_ans_ques FOREIGN KEY (Question_id) REFERENCES Question(Question_id)
 );
 SELECT * FROM Answer;
 INSERT INTO Answer( Content, Question_id, isCorrect)
@@ -165,7 +172,8 @@ Title 		VARCHAR(50),
 Category_id INT,
 Duration 	TIME,
 Creator_id 	INT,
-Create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+Create_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+CONSTRAINT fk_Ex_Cat FOREIGN KEY (Category_id) REFERENCES CategoryQuestion(Category_id)
 );
 SELECT * FROM Exam;
 INSERT INTO Exam (Code, Title, Duration, Category_id, Creator_id)
@@ -180,7 +188,9 @@ INSERT INTO Exam (Code, Title, Duration, Category_id, Creator_id)
 DROP TABLE IF EXISTS ExamQuestion;
 CREATE TABLE ExamQuestion(
 Exam_id 	INT,
-Question_id INT
+Question_id INT,
+CONSTRAINT fk_eq_ex FOREIGN KEY (Exam_id) REFERENCES Exam(Exam_id),
+CONSTRAINT fk_eq_ques FOREIGN KEY (Question_id) REFERENCES Question(Question_id)
 );
 SELECT * FROM ExamQuestion; 
 INSERT INTO ExamQuestion (Exam_id, Question_id)
